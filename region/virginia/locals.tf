aacd0333041437dea63a1c5655d84d5517e19c00
Billy <wahrsager@tutanota.de>
2023-09-11 08:41:56 -0400

    update

--- region/virginia/locals.tf
+++ region/virginia/locals.tf
@@ -29,7 +29,7 @@ locals {
 
   tags = {
     "Environment" : "Development"
-    "Project" : "Infrastructure"
+    "Project" : "EKSInfrastructure"
   }
 
 }
--- region/virginia/main.tf
+++ region/virginia/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "Networking" {
   source                     = "../../module/networking"
-  name                       = "VPC"
+  name                       = "EksVPC"
   availability_zones         = local.availability_zones
   vpc_cidr_block             = "10.0.0.0/16"
   public_subnets_cidr_block  = ["10.0.32.0/24", "10.0.96.0/24", "10.0.224.0/24"]
