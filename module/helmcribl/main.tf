16de4d8f370dcd8fb7def30fa6f0c78651ec1a4e
Billy <wahrsager@tutanota.de>
2023-09-11 18:50:35 -0400

    u

--- module/helmcribl/main.tf
+++ module/helmcribl/main.tf
@@ -3,10 +3,10 @@ resource "kubernetes_storage_class" "ebs-sc" {
     name = "ebs-sc"
   }
 
-  storage_provisioner = "kubernetes.io/aws-ebs"
+  storage_provisioner = "kubernetes.io/aws-ebs"   # original value "kubernetes.io/aws-ebs"
 
   parameters = {
-    type = "gp2"
+    type = "gp2"           # original value gp2
   }
 
   reclaim_policy = "Retain"
